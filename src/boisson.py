6a788f81ce61329575c887291a990dc264e67c2a
Clement Soulier <clement.soulier12@gmail.com>
2026-03-30 15:19:49 +0200

    ajout de la classe boisson

---
+++ src/boisson.py
@@ -0,0 +1,6 @@
+class Boisson:
+    def __init__(self, name, price, ingredients, alcohol):
+        self.name = name
+        self.price = price
+        self.ingredients = ingredients
+        self.homemade = alcohol
